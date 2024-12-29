343e26bdef9b1e153fe2f561a2ebf33d0a6cd1b0
Clancy Mendonca <clancymendonca@gmail.com>
2024-12-30 04:01:16 +0530

    Syntaxia 2025

---
+++ js/.-85-script.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    // Transition effect for navbar 
+    $(window).scroll(function() {
+      // checks if window is scrolled more than 500px, adds/removes solid class
+      if($(this).scrollTop() > 50) { 
+          $('.navbar').addClass('solid');
+      } else {
+          $('.navbar').removeClass('solid');
+      }
+    });
+}); 
+
+$(document).ready(function() {
+    const checkbox = $('input[type="checkbox"]');
+    const imageElement = $('#hamburger'); 
+    
+    // Bind event listener for checking the box
+    checkbox.on("change", function() {
+        if (this.checked) {
+            // Set new source URL when checked
+            imageElement.attr("src", "images/cross.webp"); // Replace with desired URL
+            $('.navbar').addClass('solid');
+            imageElement.css('margin-top','-0.5rem');
+        } else {
+            // Reset source URL when unchecked
+            imageElement.attr("src", "images/hamburger.webp"); // Replace with original URL
+            $('.navbar').removeClass('solid');
+            imageElement.css('margin-top','unset');
+        }
+    });
+});
